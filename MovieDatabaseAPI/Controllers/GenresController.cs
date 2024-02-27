e600570cbefceae656d2069074358b6f42bf0e4f
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-02-28 00:36:44 +0100

    Add Genres controller

---
+++ MovieDatabaseAPI/Controllers/GenresController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieDatabaseAPI.Data;
+using MovieDatabaseAPI.DTOs;
+
+namespace MovieDatabaseAPI.Controllers
+{
+    public class GenresController : BaseApiController
+    {
+        private readonly DataContext _dataContext;
+        private readonly IMapper _mapper;
+
+        public GenresController(DataContext dataContext, IMapper mapper)
+        {
+            _dataContext = dataContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<GenreDto>> GetGenres()
+        {
+            var genres = await _dataContext.Genres.ToListAsync();
+
+            return _mapper.Map<IEnumerable<GenreDto>>(genres);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<GenreDto> GetGenre(Guid id)
+        {
+            var genre = await _dataContext.Genres
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return _mapper.Map<GenreDto>(genre);
+        }
+    }
+}
